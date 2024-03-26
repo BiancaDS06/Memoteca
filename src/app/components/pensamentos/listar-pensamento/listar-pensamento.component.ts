0abe276dcdca8d18ab3663ee5c5704477ac1eb54
Bianca Dias <97123676+BiancaDS06@users.noreply.github.com>
2024-03-26 09:49:50 -0300

    subindo projeto

---
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento/pensamento';
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+  listarPensamentos: Pensamento[] = [];
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
